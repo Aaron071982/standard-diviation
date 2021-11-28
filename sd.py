481d8ff9a190546698ba99cf3d51fe8cd1da620a
Aaron071982 <91394870+Aaron071982@users.noreply.github.com>
2021-11-27 19:59:39 -0500

    Add files via upload

---
+++ sd.py
@@ -0,0 +1,41 @@
+import csv
+import math
+
+with open("class1.csv", newline="") as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+file_data.pop(0)
+
+data = file_data[0]
+
+#mean
+def mean(data):
+    n = len(data)
+    total = 0
+    for x in data:
+        total += int(x)
+
+    mean = total/n
+    return mean
+
+
+#squaring values
+squared_list = []
+for number in data:
+    a  = int(number) - mean(data)
+    a = a**2
+    squared_list.append(a)
+
+#getting sum
+sum = 0
+for i in squared_list:
+    sum = sum+i
+
+#diving sum for total values
+result = sum/(len(data)-1)
+
+#getting the deviations
+std_deviation = math.sqrt(result)
+print(std_deviation)
+
